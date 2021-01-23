9a066894280eb096c984f3216361f4f207511339
David Badalyan <d.badalyan@mail.utoronto.ca>
2021-01-23 02:27:20 -0500

    🩹 Make validation middleware stronger
    Validation middleware now reshapes req.body based on the DTO of the user

--- src/middlewares/validation.middleware.ts
+++ src/middlewares/validation.middleware.ts
@@ -5,11 +5,13 @@ import HttpException from '../exceptions/HttpException';
 
 const validationMiddleware = (type: any, value: string | 'body' | 'query' | 'params' = 'body', skipMissingProperties = false): RequestHandler => {
   return (req, res, next) => {
-    validate(plainToClass(type, req[value]), { skipMissingProperties }).then((errors: ValidationError[]) => {
+    const dto = plainToClass(type, req[value]);
+    validate(dto, { skipMissingProperties, whitelist: true }).then((errors: ValidationError[]) => {
       if (errors.length > 0) {
         const message = errors.map((error: ValidationError) => Object.values(error.constraints)).join(', ');
         next(new HttpException(400, message));
       } else {
+        req.body = dto;
         next();
       }
     });
