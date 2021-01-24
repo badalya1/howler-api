93c1848d930d261eb19d2d66879018e0d4a69844
David Badalyan <d.badalyan@mail.utoronto.ca>
2021-01-24 01:07:03 -0500

    🚧 Implement service functions

--- src/services/howls.service.ts
+++ src/services/howls.service.ts
@@ -1,4 +1,6 @@
+import { CreateHowlDto } from '../dtos/howls.dto';
 import HttpException from '../exceptions/HttpException';
+import { Howl } from '../interfaces/howls.interface';
 import howlModel from '../models/howls.model';
 import userModel from '../models/users.model';
 
@@ -6,13 +8,24 @@ class HowlsService {
   public howls = howlModel;
   public users = userModel;
 
-  public async makeHowl(userId: string, howlText: string) {
+  public async findAllHowls(): Promise<Howl[]> {
+    const howls = await this.howls.find();
+    return howls;
+  }
+  public async findHowlById(howlId: string): Promise<Howl> {
+    const howl: Howl = await this.howls.findOne({ _id: howlId });
+    if (!howl) throw new HttpException(404, `Howl with id ${howlId} was not found`);
+
+    return howl;
+  }
+  public async createHowl(userId: string, howlData: CreateHowlDto) {
+    const howlText = howlData.text;
     if (typeof howlText !== 'string' || howlText === '') throw new HttpException(400, 'Howl must contain text');
     const howl = await this.howls.create({ userId, text: howlText });
     return howl;
   }
 
-  public async getHowlsByUserId(userId: string) {
+  public async getHowlsByUserId(userId: string): Promise<Howl[]> {
     const allHowls = await this.howls.find({ userId });
     return allHowls;
   }
