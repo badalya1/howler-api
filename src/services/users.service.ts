68396457e1929b09bdde48eabdaaa87e16b3ba83
David Badalyan <d.badalyan@mail.utoronto.ca>
2021-01-24 18:25:26 -0500

    🎨 Proper error messages for users service

--- src/services/users.service.ts
+++ src/services/users.service.ts
@@ -14,24 +14,27 @@ class UserService {
 
   public async findUserById(userId: string): Promise<User> {
     const findUser: User = await this.users.findOne({ _id: userId });
-    if (!findUser) throw new HttpException(409, "You're not user");
+    if (!findUser) throw new HttpException(409, 'Cannot find user');
 
     return findUser;
   }
 
   public async updateUser(userId: string, userData: User): Promise<User> {
-    if (isEmpty(userData)) throw new HttpException(400, "You're not userData");
+    if (isEmpty(userData)) throw new HttpException(400, 'User Data must be provided');
 
     const hashedPassword = await bcrypt.hash(userData.password, 10);
     const updateUserById: User = await this.users.findByIdAndUpdate(userId, { ...userData, password: hashedPassword });
-    if (!updateUserById) throw new HttpException(409, "You're not user");
+    if (!updateUserById) throw new HttpException(409, 'Cannot find user details');
 
     return updateUserById;
   }
 
   public async deleteUserData(userId: string): Promise<User> {
+    // ? Should user be deleted or be marked as deleted
     const deleteUserById: User = await this.users.findByIdAndDelete(userId);
-    if (!deleteUserById) throw new HttpException(409, "You're not user");
+    // ? Should howls by this used be removed?
+    // await this.howls.deleteMany({userId});
+    if (!deleteUserById) throw new HttpException(409, 'Cannot find user');
 
     return deleteUserById;
   }
